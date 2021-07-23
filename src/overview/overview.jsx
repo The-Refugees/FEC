8d03f8a5ea717f0391263cf266f80d83260ff702
pjones2012 <portiamelissa90@gmail.com>
2021-07-23 11:42:53 -0500

    created overview component

---
+++ src/overview/overview.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Overview extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>Overview in the House!</div>
+    );
+  }
+}
+
+export default Overview;
