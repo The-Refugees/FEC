497227f30650a0892f4e523874a676b8fd64de1e
Jason <jasondye.music@gmail.com>
2021-07-23 11:23:48 -0500

    added babel, initialized App.jsx, adjusted webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,15 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
 };
