9c806b39ace1a4328456137eef156fcd41dcb309
Jason <jasondye.music@gmail.com>
2021-07-27 08:39:34 -0500

    initializing review components (with code)

---
+++ src/reviews/RatingsReviews.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Ratings from './Ratings.jsx';
+import Reviews from './Reviews.jsx';
+
+
+const RatingsReviews = (props) => {
+
+
+
+  return (
+    <div id="ratingsreviews">
+      Hello RatingsReviews!
+      <Ratings />
+      <Reviews />
+    </div>
+  )
+}
