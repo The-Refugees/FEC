8d833c793711bf601b04d2879b6f8e4975680d52
pjones2012 <portiamelissa90@gmail.com>
2021-07-29 10:09:43 -0500

    added quantity of reviews

--- src/overview/overview.jsx
+++ src/overview/overview.jsx
@@ -14,6 +14,8 @@ import Col from 'react-bootstrap/Col';
 function Overview (props) {
 
   const [styleInfo, setStyle] = useState({photos: []});
+  const [[ratingTotal, ratingAvg] , setRating] = useState([0,0]);
+
   const solid = 0;
   useEffect ( () => {
     var productID = props.product.id;
@@ -29,6 +31,23 @@ function Overview (props) {
     .catch( (err) => {
       console.log('Houston we have this problem: ' + err);
     });
+
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/reviews/meta?product_id=${productID}`, {headers: {Authorization: AUTH_TOKEN }})
+    .then( (response) => {
+      var total = 0;
+      var avg = 0;
+      for(var key in response.data.ratings) {
+        total += Number(response.data.ratings[key]);
+        avg += Number(key)*Number(response.data.ratings[key]);
+      }
+      avg=avg/total;
+      console.log('This is the total ' + total);
+      console.log('This is the avg ' + avg);
+      setRating([total, avg]);
+    })
+    .catch( (err) => {
+      console.log('cant calculate review total: ' + err);
+    });
   }, [solid]);
 
 
@@ -40,7 +59,7 @@ function Overview (props) {
         </Col>
         <Col sm={3} md={3} lg={3}>
           <Row>
-            <ProductInfo product={props.product} info={styleInfo}/>
+            <ProductInfo product={props.product} info={styleInfo} rating={[ratingTotal, ratingAvg]}/>
           </Row>
           <Row>
             <StyleSelector />
--- src/overview/productInfo.jsx
+++ src/overview/productInfo.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+import AUTH_TOKEN from '../config.js';
 
 var ProductInfo = (props) =>{
+
   var currentPrice = () => {
     //props.info can be used to update default price to current price here.
     return props.info.original_price;
@@ -10,8 +13,8 @@ var ProductInfo = (props) =>{
   return (
     <div>
       <div>
-        <span>Star Rating Component</span>
-        <Button variant="link">Read all reviews</Button>
+        <span>Star Rating Component = {props.rating[1].toFixed(1)}</span>
+        <Button variant="link">Read all {props.rating[0]} reviews</Button>
       </div>
       <div> {props.product.category}</div>
       <h3>{props.product.name}</h3>
