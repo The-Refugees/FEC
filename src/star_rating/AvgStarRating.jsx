97845b34bce2a8208df2403c87e0b54f98e1246a
Jason <jasondye.music@gmail.com>
2021-07-30 10:38:02 -0500

    star rating components complete

---
+++ src/star_rating/AvgStarRating.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+// expects a PropType of 'rating' to render the stars
+
+const AvgStarRating = (props) => {
+
+  let {rating} = props;
+  if (rating > 5 ||
+    rating < 1 ||
+    rating === NaN ||
+    rating === undefined) {
+    return null
+  };
+  let starFull = {
+    path: '../src/star_rating/star_img/Star_Full.png',
+    alt: 'image of a fully filled-in star'
+  };
+  let star75 = {
+    path: '../src/star_rating/star_img/Star75.png',
+    alt: 'image of a 75 percent filled-in star'
+  };
+  let starHalf = {
+    path: '../src/star_rating/star_img/Star_Half.png',
+    alt: 'image of a half filled-in star'
+  };
+  let star25 = {
+    path: '../src/star_rating/star_img/Star25.png',
+    alt: 'image of a 25 percent filled-in star'
+  };
+  let starEmpty = {
+    path: '../src/star_rating/star_img/Star_Empty.png',
+    alt: 'image of an empty star'
+  };
+
+  // round to the nearest .25
+  let rounded = (Math.round(rating * 4) / 4).toFixed(2);
+  let stars = [];
+
+  // loop 5 times
+  for (var i = 5; i > 0; i--) {
+    if (rounded >= 1) {
+      rounded--;
+      stars.push(starFull);
+    } else if (rounded === 0.75) {
+      rounded = 0;
+      stars.push(star75);
+    } else if (rounded === 0.5) {
+      rounded = 0;
+      stars.push(starHalf);
+    } else if (rounded === 0.25) {
+      rounded = 0;
+      stars.push(star25);
+    } else if (rounded === 0) {
+      stars.push(starEmpty);
+    }
+  }
+  console.log(stars);
+
+  return (
+    <Row>
+      {stars.map((star, index) => {
+        return (
+          <Col sm={1} key={'star' + index}>
+            <img
+              src={star.path}
+              width="30px"
+              height="30px"
+              alt={star.alt}
+            />
+          </Col>
+        )
+      })}
+    </Row>
+  );
+}
+
+export default AvgStarRating;
