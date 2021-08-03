5978d54df97f2fca5ae5c284a98534b933491bde
chrisdwoodby <chrisdwoodby@gmail.com>
2021-08-03 12:15:34 -0500

    list of answers does not contain repeated answers

--- src/questions/Question.jsx
+++ src/questions/Question.jsx
@@ -11,23 +11,30 @@ import QuestionForm from './QuestionForm.jsx';
 
 function Question(props) {
   var first2 = [];
-  var inner = function() {
+  var getFirst2Answers = function() {
   Object.keys(props.a).map((key, i) => {
     if (first2.length !== 2) {
       first2.push(props.a[key])
     }
+    for (var i = 0; i < first2.length; i++) {
+      for (var keys in props.a) {
+        if (props.a[key] === first2[i]) {
+          delete props.a[key]
+        }
+      }
+    }
     return first2
   })
 }
-inner()
+getFirst2Answers()
 
 
   return (
     <Container id="question" >
-      { 'Q: ' + props.q}
+    <Card style={{padding: '10px', backgroundColor: "rgb(214,234,248)"}}>{ 'Q: ' + props.q}</Card>
       {first2.map((answer, i) => (
         <Container key={i}>
-          <Row style={{padding: '12px', backgroundColor: "rgb(192,192,192)"}} >{'A: ' + answer.body}</Row>
+          <Row style={{padding: '12px', backgroundColor: "rgb(242,244,244)"}} >{'A: ' + answer.body}</Row>
           <Row >{'by ' + answer.answerer_name + ' ' + dateParser(answer.date) + '   ' + 'Helpful?'}</  Row>
         </Container>
       ))}
@@ -36,7 +43,7 @@ inner()
 
             <Accordion >
             <Accordion.Item eventKey="0">
-              <Accordion.Header>Load More Answers</Accordion.Header>
+
               <Accordion.Body>
                 {!props.loading &&
                 <Container>
@@ -46,7 +53,7 @@ inner()
 
                      {Object.keys(props.a).map((key, i) => (
                        <Card key={i}>
-                         <Row style={{padding: '12px', backgroundColor: "rgb(192,192,192)"}}>{'A: ' + props.a[key].body}</Row>
+                         <Row style={{padding: '12px', backgroundColor: "rgb(242,244,244)"}}>{'A: ' + props.a[key].body}</Row>
                          <Row >{'by ' + props.a[key].answerer_name + ' ' + dateParser(props.a[key].date)}</Row>
                        </Card>
                       ))}
@@ -55,6 +62,7 @@ inner()
                 </Container>
                 }
               </Accordion.Body>
+              <Accordion.Header>Load More Answers</Accordion.Header>
             </Accordion.Item>
           </Accordion>
       }
