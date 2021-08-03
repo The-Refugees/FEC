558d17d7cfa6ae9a73c3872ae8385c479038950f
pjones2012 <portiamelissa90@gmail.com>
2021-08-03 11:46:29 -0500

    Serverwork

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var axios = require('axios');
+//var AUTH_TOKEN = require('./config.js');
+
 var app = express();
 var port = 3001;
 
@@ -6,6 +9,17 @@ app.use(express.json());
 console.log(__dirname + '/../dist')
 app.use(express.static(__dirname + '/../dist'));
 
+app.get('/products', (req, res)=> {
+
+  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/`, {headers: {Authorization: AUTH_TOKEN }})
+    .then( (response) => {
+      res.status(200).send(response.data);
+    })
+    .catch( (err) => {
+      console.log('am I getting this error? ', err);
+      res.status(404).send('Cannot get initial Product Data');
+    });
+})
 
 app.listen(port, () => {
   console.log(`listening on port ${port}!`);
--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,51 @@ import QandA from './questions/QandA.jsx';
 import RatingsReviews from './reviews/RatingsReviews.jsx';
 import data from './overview/data.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      term: '',
+      currentProductIndex: 0,
+      productList: [{id:24156, category: '', slogan: '', name: ''}]};
   }
 
+  componentDidMount() {
+    console.log("Hello bud what are we doing?");
+    axios.get(`http://localhost:3001/products`)
+    .then( (response) => {
+      console.log("setting the app state :) ", response.data)
+      this.setState({productList: response.data});
+    })
+    .catch( (err) => {
+      console.log(err);
+    });
+  }
+
+  handleProductSubmit = (e) => {
+    e.preventDefault();
+    console.log('is this the right thing: ', this.state.term);
+    this.setState({currentProductIndex: this.state.term, term: ''})
+  }
+
+  inputHandler = (e) => {
+    e.preventDefault();
+    console.log('is this the right thing: ', e.target.input);
+    this.setState({term: e.target.value})
+  }
 
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
-        <Overview product={data[0]}/>
+        <h1 >Hello User! </h1>
+        <form onSubmit={this.handleProductSubmit}>
+          What index do you want to see?
+          <input type="text" name="productIndex" value={this.state.term} onChange={this.inputHandler}/>
+          <button type="submit"  value="Get Product"/>
+        </form>
+        <Overview product={this.state.productList[this.state.currentProductIndex]}/>
         <QandA/>
         <RatingsReviews />
       </div>
