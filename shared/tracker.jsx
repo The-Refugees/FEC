f1409c922e971800788154d0cda9bb4f7d1c3209
pjones2012 <portiamelissa90@gmail.com>
2021-08-05 10:08:17 -0500

    Tracker also loging time

--- shared/tracker.jsx
+++ shared/tracker.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import date from 'date-and-time';
+const now = new Date();
 
 const Tracker = (props) => {
   var trackerHandler = (e) => {
-    console.log("Big brother sees you!\n Element: ", e.target);
+    console.log("Big brother sees you!\nTime: ",date.format(now, 'ddd, MMM DD YYYY HH:mm:ss') ,"\nElement: ", e.target);
   }
 
   return (
